e16fc042ef7e5e62cf16b1990cdcae149d8e4b36
BradfordsCommission <71363709+BradfordsCommission@users.noreply.github.com>
2021-03-24 22:28:06 -0400

    Delete File Reading Tests.py

--- File Reading Tests.py
+++
@@ -1,7 +0,0 @@
-#fetching the file from my computer, and telling the file that i'd like to write something in it
-DNA_File_Name = "DNA.txt"
-DNA_File = open(DNA_File_Name, "w")
-
-#the text I'm filling the file with
-DNA_File.write("Writing code is not hard")
-DNA_File.close()
